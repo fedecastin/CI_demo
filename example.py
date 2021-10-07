5279e679af5cb7a87019aed492d3184fe9044085
fedecastin <91869307+fedecastin@users.noreply.github.com>
2021-10-07 12:17:20 +0200

    Include test

--- example.py
+++ example.py
@@ -1,3 +1,10 @@
 temperature = 10
 def print_temperature():
     print(temperature)
+
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
